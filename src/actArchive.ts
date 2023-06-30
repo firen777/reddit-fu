22e44dec975ddb3cd9213cc0c5750f7b99bfa10d
firen777 <hivemindac1995@gmail.com>
2023-06-30 15:45:41 +0800

    tested navigation

--- src/actArchive.ts
+++ src/actArchive.ts
@@ -10,35 +10,66 @@ export const archive = async (page:Page)=>{
   //   await fs.promises.mkdir(archiveDir);
   // }
 
-  const editBtns = await page.$$('a.edit-usertext');
+  const editBtns = await page.$$('a.edit-usertext:not([style*="display:none"])')
   for (const e of editBtns) {
-    console.log("DEBUG: editBtns iteration", e);
     try {
       await e.click();
     } catch (error) {
       console.error("ERR: not clickable. Probably already clicked.");
       console.error(error);
+    } finally {
+      await e.dispose();
     }
   }
-  
-  // const editAreas = await page.$$eval('div.usertext-edit textarea', list=>list.map(t=>t.value));
-  // for (const area of editAreas) {
-  //   console.log("DEBUG: ", area);
-  // }
+
+  const expandBtns = await page.$$('div.expando-button:not(.expanded)')
+  for (const e of expandBtns) {
+    try {
+      await e.click();
+    } catch (error) {
+      console.error("ERR: not clickable. Probably already clicked.");
+      console.error(error);
+    } finally {
+      await e.dispose();
+    }
+  }
 
   const divThings = await page.$$('div.thing');
   const archivedContent = divThings.map(async (d)=>{
+    const isComment = await d.evaluate(el=>el.classList.contains('comment'));
+
     const permalink = await d.$(`a[data-event-action="permalink"]`)
+      .then((a)=>a?a.getProperty('href'):null)
+      .then((href)=>href?href.jsonValue():'');
+    const context = await d.$(`a[data-event-action="context"]`)
       .then((a)=>a?a.getProperty('href'):null)
       .then((href)=>href?href.jsonValue():'');
     const comments = await d.$(`a[data-event-action="comments"]`)
       .then((a)=>a?a.getProperty('href'):null)
       .then((href)=>href?href.jsonValue():'');
-      
+    const timePost = await d.$(`time:not(.edited-timestamp)`)
+      .then((t)=>t?t.getProperty('dateTime'):null)
+      .then((d)=>d?d.jsonValue():'');
+    const timeEdit = await d.$(`time.edited-timestamp`)
+      .then((t)=>t?t.getProperty('dateTime'):null)
+      .then((d)=>d?d.jsonValue():'');
+    const text = await d.$(`div.usertext-edit textarea`)
+      .then((t)=>t?t.getProperty('innerHTML'):null)
+      .then((t)=>t?t.jsonValue():"");
+    const score = await d.$(`.score.likes`)
+      .then((t)=>t?t.getProperty('title'):null)
+      .then((d)=>d?d.jsonValue():'');
+
+    await d.dispose();
     return {
       permalink,
+      context,
       comments,
-
+      timePost,
+      timeEdit,
+      text,
+      score,
+      isComment,
     }
   })
 
@@ -53,7 +84,18 @@ const main = async ()=>{
   const pages = await browser.browserContexts()[0].pages();
   const redditPage = pages.find((p)=>(!!p.url().match('https://old.reddit.com/user/firen777')));
   if (redditPage) {
+    let count = 0;
+    let nxtBtn:puppeteer.ElementHandle<HTMLSpanElement>|null = await redditPage.$('span.next-button');
     await archive(redditPage);
+    while (!!nxtBtn && count<3) {
+      await Promise.all([
+        redditPage.waitForNavigation(),
+        nxtBtn.click(),
+      ])
+      await archive(redditPage);
+      nxtBtn = await redditPage.$('span.next-button');
+      count++;
+    }
   }
 
   browser.disconnect();
